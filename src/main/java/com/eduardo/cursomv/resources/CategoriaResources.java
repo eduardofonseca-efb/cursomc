fed5c90bf54f720968005eabb36db22c6e8b0904
eduardo fonseca <edufonsecabarbosa@hotmail.com>
2020-02-04 22:14:12 -0300

    testando a primeira classe de dominio, categoria

--- src/main/java/com/eduardo/cursomv/resources/CategoriaResources.java
+++ src/main/java/com/eduardo/cursomv/resources/CategoriaResources.java
@@ -1,15 +1,27 @@
 package com.eduardo.cursomv.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.eduardo.cursomv.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "esta funcionando";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"nformatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista =  new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 
 }
